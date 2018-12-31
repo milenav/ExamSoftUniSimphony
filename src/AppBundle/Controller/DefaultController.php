535584b29ed41a1082a31df78aa165844f6798eb
Milena Valcheva <milena_77@mail.bg>
2018-12-31 19:47:05 +0200

    change file

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,9 +15,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $products = $this->getDoctrine()->getRepository('AppBundle:Product')->findAll();
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+                'products' => $products
         ]);
     }
 }
